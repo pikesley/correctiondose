7aedce7b27d3328961eff756d656202dc2a1e48b
pikesley <github@orgraphone.org>
2016-01-27 16:50:28 +0000

    This is about to get messy

--- spec/controllers/glucose_measurement_controller_spec.rb
+++ spec/controllers/glucose_measurement_controller_spec.rb
@@ -5,9 +5,55 @@ describe GlucoseMeasurementController, type: :controller do
 
   describe 'GET #index' do
     it 'populates an array of measurements' do
-      measurement = FactoryGirl.create(:glucose_measurement)
+      measurement = create(:glucose_measurement)
       get :index
       expect(assigns(:measurements)).to eq [measurement]
+    end
+
+    it 'renders the #index view' do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+    it 'assigns the requested measurement to @measurement' do
+      measurement = create(:glucose_measurement)
+      get :show, id: measurement
+      expect(assigns(:measurement)).to eq measurement
+    end
+
+    it 'renders the #show view' do
+      get :show, id: create(:glucose_measurement)
+      expect(response).to render_template :show
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'creates a new measurement' do
+        expect {
+          post :create, measurement: attributes_for(:glucose_measurement)
+        }.to change(GlucoseMeasurement, :count).by 1
+      end
+
+      it 'redirects to the new measurement' do
+        post :create, measurement: attributes_for(:glucose_measurement)
+        expect(response).to redirect_to GlucoseMeasurement.last
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the new contact' do
+        expect {
+          post :create, measurement: attributes_for(:invalid_glucose_measurement)
+        }.to_not change(GlucoseMeasurement, :count)
+      end
+
+      it 're-renders the #new view' do
+        post :create, measurement: attributes_for(:invalid_glucose_measurement)
+        expect(response).to render_template :new
+      end
     end
   end
 end
--- spec/factories/glucose_measurements.rb
+++ spec/factories/glucose_measurements.rb
@@ -1,6 +1,10 @@
 FactoryGirl.define do
-  factory :glucose_measurement do
-    time '2016-01-26 21:42:29'
-    value 6.0
+  factory :glucose_measurement do |f|
+    f.time '2016-01-26 21:42:29'
+    f.value 6.0
+  end
+
+  factory :invalid_glucose_measurement, parent: :glucose_measurement do |f|
+    f.value -1
   end
 end

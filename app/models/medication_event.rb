6e6c088fccf10b039683c34739cab63bc8e9eea6
pikesley <github@orgraphone.org>
2016-01-28 22:30:41 +0000

    Colours

--- app/models/medication_event.rb
+++ app/models/medication_event.rb
@@ -1,6 +1,12 @@
 class MedicationEvent < ActiveRecord::Base
-# this didn't work http://stackoverflow.com/questions/14113057/how-to-have-a-drop-down-select-field-in-a-rails-form
+  def self.insulins
+    [
+      'humalog',
+      'lantus'
+    ]
+  end
+
   validates :datetime, presence: true
   validates :amount, presence: true, numericality: { greater_than: 0 }
-  validates :insulin, presence: true, inclusion: { in: [ 'humalog', 'lantus' ] }
+  validates :insulin, presence: true, inclusion: { in: insulins }
 end

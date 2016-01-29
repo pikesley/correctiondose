ce8c4ef06496f0fbf6e73927386e783d06ca3a81
pikesley <github@orgraphone.org>
2016-01-29 23:59:12 +0000

    Tidy up form

--- app/models/glucose_measurement.rb
+++ app/models/glucose_measurement.rb
@@ -1,4 +1,10 @@
 class GlucoseMeasurement < ActiveRecord::Base
+  def self.fields
+    [
+      'value'
+    ]
+  end
+
   validates :datetime, presence: true
   validates :value, presence: true, numericality: { greater_than: 0 }
 end

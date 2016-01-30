60cc86f66401717eedc229e897194492885b7c98
pikesley <github@orgraphone.org>
2016-01-30 15:52:55 +0000

    Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160129223328) do
+ActiveRecord::Schema.define(version: 20160130154543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,14 @@ ActiveRecord::Schema.define(version: 20160129223328) do
     t.float    "dose"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "physical_exercises", force: :cascade do |t|
+    t.datetime "datetime"
+    t.float    "duration"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|

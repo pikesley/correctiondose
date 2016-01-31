3111b1ccceb5ce9bcce26af88c4cb1d3896680ef
pikesley <github@orgraphone.org>
2016-01-31 14:43:36 +0000

    Tests for front-page

--- spec/requests/welcome_spec.rb
+++ spec/requests/welcome_spec.rb
@@ -19,8 +19,32 @@ describe 'Welcome' do
           'Add Exercise'
         ]
       end
+    end
 
-      
+    before :each do
+      DatabaseCleaner.clean
+
+      Timecop.freeze 2015, 01, 31, 10, 00
+      @metrics = [
+        create(:glucose_measurement, datetime: Time.now - 3600),
+        create(:glucose_measurement, datetime: Time.now - 7200),
+        create(:medication_event, datetime: Time.now - 5400),
+        create(:carbohydrate_intake, datetime: Time.now - 1800)
+      ]
+    end
+
+    after :each do
+      Timecop.return
+    end
+
+    it 'has a table of datas' do
+      visit root_path(as: user)
+      within 'table' do
+        expect(page).to have_content '09:30	Carbs	70.0 pasta bolognese'
+        expect(page).to have_content '09:00	Glucose	6.0'
+        expect(page).to have_content '08:30	Meds 10.0	humalog'
+        expect(page).to have_content '08:00	Glucose	6.0'
+      end
     end
   end
 end

f3988cc1a1a5cb34f50bcfd6936f447c77b24b71
pikesley <github@orgraphone.org>
2016-01-31 13:46:19 +0000

    Wow it works

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,12 @@
 class WelcomeController < ApplicationController
   def index
+    @metrics = []
+    [
+      GlucoseMeasurement,
+      CarbohydrateIntake,
+      MedicationEvent
+    ].each do |model|
+      @metrics.concat model.where(datetime: (Time.now - 48.hours)..Time.now)
+    end
   end
 end

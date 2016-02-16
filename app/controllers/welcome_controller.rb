7aceeeb2d1904f775a027369d3a348af7c503942
pikesley <github@orgraphone.org>
2016-02-16 23:29:04 +0000

    Fix default

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   before_action :require_login
 
   def index
-    @hours = 36
+    @hours = 72
     if params[:hours]
       @hours = hours(params[:hours]) ? hours(params[:hours]) : @hours
     end

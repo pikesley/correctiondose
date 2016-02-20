226338c37cc0a8180c0f0b02f6edda4248494006
pikesley <github@orgraphone.org>
2016-02-20 21:56:55 +0000

    Factor stuff out

--- spec/models/glucose_measurement_spec.rb
+++ spec/models/glucose_measurement_spec.rb
@@ -6,4 +6,8 @@ describe GlucoseMeasurement, type: :model do
   it 'is invalid with a value less than zero' do
     expect(build :glucose_measurement, value: -1).to_not be_valid
   end
+
+  it 'represents itself as an array of table cells' do
+    
+  end
 end

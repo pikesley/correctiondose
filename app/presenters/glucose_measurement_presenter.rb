ec68d122c5d742979fc07e5f8b243fffb594593f
pikesley <github@orgraphone.org>
2016-02-22 19:00:45 +0000

    to_tr working as desired

--- app/presenters/glucose_measurement_presenter.rb
+++ app/presenters/glucose_measurement_presenter.rb
@@ -1,8 +1,68 @@
 require 'delegate'
 
 class GlucoseMeasurementPresenter < SimpleDelegator
-  def fancy_value
-    @fancy_value ||= "The value is #{model.value}"
+  # http://stackoverflow.com/questions/6074831/how-to-access-url-helper-from-rails-module
+  delegate :url_helpers, to: 'Rails.application.routes'
+
+  # https://christoph.luppri.ch/articles/2015/07/04/handmade-rails-presenters/
+  include ActionView::Helpers::UrlHelper
+
+  def edit_cell
+    cell(link_to model.datetime.strftime('%H:%M'),
+    url_helpers.edit_glucose_measurement_path(model),
+    title: 'Edit this measurement')
+  end
+
+  def label_cell
+    cell model.class.short_name
+  end
+
+  def number_span
+    "<span class='number'>#{model.value}</span>"
+  end
+
+  def units_span
+    "<span class='units'>#{units[:short]}</span>"
+  end
+
+  def value_div
+    "<div class='value' data-toggle='tooltip' data-placement='top' title='#{model.value} #{units[:full]}'>#{number_span} #{units_span}</div>"
+  end
+
+  def measurement_cell
+    cell value_div, 'glucose-measurement-value'
+  end
+
+  def filler_cell
+    cell '', 'filler'
+  end
+
+  def to_tr padding: 0
+    s = "<tr>#{edit_cell}#{label_cell}#{measurement_cell}"
+
+    padding.times do
+      s += filler_cell
+    end
+
+    s += "</tr>"
+
+    s
+  end
+
+  def cell content, css_class = nil
+    s = nil
+    if css_class
+      s = " class='#{css_class}'"
+    end
+
+    "<td#{s}>#{content}</td>"
+  end
+
+  def units
+    {
+      full: 'millimoles per Litre',
+      short: 'mmol/L'
+    }
   end
 
   def model
--- spec/presenters/glucose_measurement_presenter_spec.rb
+++ spec/presenters/glucose_measurement_presenter_spec.rb
@@ -1,8 +1,46 @@
 describe GlucoseMeasurementPresenter do
+  before :each do
+    DatabaseCleaner.clean
+  end
+
   let(:bg) { create :glucose_measurement }
   let(:decorated_bg) { GlucoseMeasurementPresenter.new bg }
 
-  it 'gives the correct value' do
-    expect(decorated_bg.fancy_value).to eq 'The value is 6.0'
+  it 'has an Edit cell' do
+    expect(decorated_bg.edit_cell).to eq '<td><a title="Edit this measurement" href="/glucose/1/edit">21:42</a></td>'
+  end
+
+  it 'has a Label cell' do
+    expect(decorated_bg.label_cell).to eq '<td>Glucose</td>'
+  end
+
+  it 'has a Number span' do
+    expect(decorated_bg.number_span).to eq "<span class='number'>6.0</span>"
+  end
+
+  it 'has a Units span' do
+    expect(decorated_bg.units_span).to eq "<span class='units'>mmol/L</span>"
+  end
+
+  it 'has a Value div' do
+    expect(decorated_bg.value_div).to eq (
+      "<div class='value' data-toggle='tooltip' data-placement='top' title='6.0 millimoles per Litre'><span class='number'>6.0</span> <span class='units'>mmol/L</span></div>"
+    )
+  end
+
+  it 'presents as a whole table row' do
+    expect(decorated_bg.to_tr).to eq (
+      "<tr><td><a title=\"Edit this measurement\" href=\"/glucose/1/edit\">21:42</a></td><td>Glucose</td><td class='glucose-measurement-value'><div class='value' data-toggle='tooltip' data-placement='top' title='6.0 millimoles per Litre'><span class='number'>6.0</span> <span class='units'>mmol/L</span></div></td></tr>"
+    )
+  end
+
+  it 'pads the row if required' do
+    expect(decorated_bg.to_tr padding: 1).to eq (
+      "<tr><td><a title=\"Edit this measurement\" href=\"/glucose/1/edit\">21:42</a></td><td>Glucose</td><td class='glucose-measurement-value'><div class='value' data-toggle='tooltip' data-placement='top' title='6.0 millimoles per Litre'><span class='number'>6.0</span> <span class='units'>mmol/L</span></div></td><td class='filler'></td></tr>"
+    )
+  end
+
+  it 'has a Measurement cell' do
+    expect(decorated_bg.measurement_cell).to eq "<td class='glucose-measurement-value'><div class='value' data-toggle='tooltip' data-placement='top' title='6.0 millimoles per Litre'><span class='number'>6.0</span> <span class='units'>mmol/L</span></div></td>"
   end
 end

5b7f30370c7bae59fe3181eee83e4d373e5c17b1
pikesley <github@orgraphone.org>
2016-02-23 20:08:19 +0000

    Almost works

--- app/controllers/longterm_controller.rb
+++ app/controllers/longterm_controller.rb
@@ -3,8 +3,13 @@ class LongtermController < ApplicationController
 
   def index
     @metrics = [
-      GlycatedHaemoglobin.all,
-      BloodPressure.all
+      GlycatedHaemoglobin,
+      BloodPressure
+    ]
+
+    @datas = [
+      ControllerHelpers.for_table(GlycatedHaemoglobin.all),
+      ControllerHelpers.for_table(BloodPressure.all)
     ]
   end
 end

c0760c781694c9bc35f36cc13db1dde9f9ae0c2f
pikesley <github@orgraphone.org>
2016-02-27 23:15:21 +0000

    This somewhat works
    I need to introduce Cells

--- app/controllers/generic_controller.rb
+++ app/controllers/generic_controller.rb
@@ -9,7 +9,14 @@ class GenericController < ApplicationController
 
     @model = find_class
     @metrics = @model.where(datetime: (Time.now - @hours.hours)..Time.now)
+
+    if @metrics == []
+      @metrics = @model.first(3)
+    end
+
     @bucketed_metrics = @metrics.group_by { |g| g.datetime.strftime "%Y-%m-%d" }
+
+    @with_year = @metrics.first.datetime.year < Time.now.year
 
     @has_charts = false
   end
